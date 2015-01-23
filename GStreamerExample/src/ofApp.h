dacf86c6a2cea9be3667f4c6061e47125950f8ad
Vladimir Gusev <bo-ba@mail.ru>
2015-01-23 00:12:10 -0500

    add gstreamer example

---
+++ GStreamerExample/src/ofApp.h
@@ -0,0 +1,49 @@
+/*
+ * ofApp.h
+ *
+ *  Created on: Jul 19, 2013
+ *      Author: arturo castro
+ */
+
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGstRTPClient.h"
+#include "ofxGstRTPServer.h"
+#include "ofxGui.h"
+
+#define DO_ECHO_CANCEL 0
+
+#if DO_ECHO_CANCEL
+#include "ofxEchoCancel.h"
+#endif
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    ofxGstRTPClient client;
+    ofxGstRTPServer server;
+    
+    ofVideoGrabber grabber;
+    ofTexture remoteVideo;
+    
+    ofxPanel gui;
+#if DO_ECHO_CANCEL
+    ofxEchoCancel echoCancel;
+#endif
+};
---
+++ LSystem-master/README.md
@@ -0,0 +1,18 @@
+## LSystems - CLOUDS Visual System
+###Author [Patricio Gonzalez Vivo](www.patriciogonzalezvivo.com)
+
+####Presets & KeyWords
+
+* Preset_1: Keyword_1, Keyword_2, Keyword_3
+* Preset_2: Keyword_1, Keyword_2, Keyword_3 
+* Preset_3: Keyword_1, Keyword_2, Keyword_3 
+
+
+
+ 
+
+
+
+
+
+
